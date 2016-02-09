7c8b6fe52fc15648c5ac5aa1f76934e71bedf99d
GR <484849@CTS.COM>
2016-02-09 16:07:36 +0530

    Added procfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end("Seetha started");
+    res.end("Seetha started raman");
 })
 
 server.listen(process.env.PORT || 5000,function(){console.log("server.started")});
